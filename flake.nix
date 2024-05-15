ffd0dcb6becbd9fc8be5eef4ecd16558dba07996
diamondburned <diamond@libdb.so>
2024-05-15 16:57:50 -0700

    Add devShells.default with formatter

--- flake.nix
+++ flake.nix
@@ -1,30 +1,49 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	outputs = { self, nixpkgs, flake-utils }:
-		flake-utils.lib.eachDefaultSystem (system: let
-			pkgs = nixpkgs.legacyPackages.${system};
-		in
-		{
-			packages = rec {
-				default = dnsmill;
-				dnsmill = pkgs.buildGoModule {
-					pname = "dnsmill";
-					version = self.rev or "unknown";
-					src = self;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go_1_21
+            gopls
+            gotools
+            self.formatter.${system}
+          ];
+        };
 
-					vendorHash = "sha256-kZPHwO/Hxe3cSmRqzq2d0ESf+V20T1miQ+ZMN21bs+g=";
+        packages = rec {
+          default = dnsmill;
+          dnsmill = pkgs.buildGoModule {
+            pname = "dnsmill";
+            version = self.rev or "unknown";
+            src = self;
 
-					meta = {
-						description = "Declaratively set your DNS records with dnsmill, powered by libdns.";
-						homepage = "https://libdb.so/dnsmill";
-						license = pkgs.lib.licenses.isc;
-						mainProgram = "dnsmill";
-					};
-				};
-			};
-		});
+            vendorHash = "sha256-kZPHwO/Hxe3cSmRqzq2d0ESf+V20T1miQ+ZMN21bs+g=";
+
+            meta = {
+              description = "Declaratively set your DNS records with dnsmill, powered by libdns.";
+              homepage = "https://libdb.so/dnsmill";
+              license = pkgs.lib.licenses.isc;
+              mainProgram = "dnsmill";
+            };
+          };
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
